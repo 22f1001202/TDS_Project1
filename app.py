dca90e8e8cdb7340968242f23b6695cb1a2f01eb
22f1001202 <22f1001202@ds.study.iitm.ac.in>
2025-02-15 20:45:43 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-# This is the project file 
+# /// script 
+# requires-python = ">=3.11"
+# dependencies = [
+#     "fastapi",
+#     "uvicorn",
+#     "requests"
+# ]
+# ///
+
+from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware  
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True,
+    allow_methods = ['GET', 'POST'],
+    allow_headers = ["*"]
+)
+
+@app.get("/")
+def home():
+    return {"hello world"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
